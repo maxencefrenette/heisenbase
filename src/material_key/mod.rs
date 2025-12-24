1f88996dc39439484f5d1e408c44118e1e1ee56c
Maxence Frenette <maxence.frenette@gmail.com>
2025-12-24 09:25:48 -0500

    Refactor material_key into its own folder

--- src/material_key/mod.rs
+++ src/material_key/mod.rs
@@ -3,86 +3,9 @@ use std::fmt;
 
 use shakmaty::{Chess, Color, Position, PositionErrorKinds, Role, Square};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum HbPieceRole {
-    King,
-    Queen,
-    Rook,
-    LightBishop,
-    DarkBishop,
-    Knight,
-    Pawn,
-}
+mod hb_piece;
 
-impl HbPieceRole {
-    fn token(self) -> &'static str {
-        match self {
-            HbPieceRole::King => "K",
-            HbPieceRole::Queen => "Q",
-            HbPieceRole::Rook => "R",
-            HbPieceRole::DarkBishop => "Bd",
-            HbPieceRole::LightBishop => "Bl",
-            HbPieceRole::Knight => "N",
-            HbPieceRole::Pawn => "P",
-        }
-    }
-
-    fn from_token(tok: &str) -> Option<Self> {
-        match tok {
-            "K" => Some(HbPieceRole::King),
-            "Q" => Some(HbPieceRole::Queen),
-            "R" => Some(HbPieceRole::Rook),
-            "Bd" => Some(HbPieceRole::DarkBishop),
-            "Bl" => Some(HbPieceRole::LightBishop),
-            "N" => Some(HbPieceRole::Knight),
-            "P" => Some(HbPieceRole::Pawn),
-            _ => None,
-        }
-    }
-
-    pub fn role(self) -> Role {
-        match self {
-            HbPieceRole::King => Role::King,
-            HbPieceRole::Queen => Role::Queen,
-            HbPieceRole::Rook => Role::Rook,
-            HbPieceRole::DarkBishop | HbPieceRole::LightBishop => Role::Bishop,
-            HbPieceRole::Knight => Role::Knight,
-            HbPieceRole::Pawn => Role::Pawn,
-        }
-    }
-
-    pub fn is_bishop(self) -> bool {
-        match self {
-            HbPieceRole::DarkBishop | HbPieceRole::LightBishop => true,
-            _ => false,
-        }
-    }
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct HbPiece {
-    pub role: HbPieceRole,
-    pub color: Color,
-}
-
-impl From<HbPiece> for shakmaty::Piece {
-    fn from(hb_piece: HbPiece) -> shakmaty::Piece {
-        shakmaty::Piece {
-            role: hb_piece.role.role(),
-            color: hb_piece.color,
-        }
-    }
-}
-
-pub const PIECES: [HbPieceRole; 7] = [
-    HbPieceRole::King,
-    HbPieceRole::Queen,
-    HbPieceRole::Rook,
-    HbPieceRole::DarkBishop,
-    HbPieceRole::LightBishop,
-    HbPieceRole::Knight,
-    HbPieceRole::Pawn,
-];
+pub use hb_piece::{HbPiece, HbPieceRole, PIECES};
 
 /// Represents a material configuration, e.g. `KQvK`.
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
