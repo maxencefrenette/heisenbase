72871bd3e1fe498a12e6476a212e9fdd4dd54866
Maxence Frenette <maxence.frenette@gmail.com>
2025-12-27 20:02:17 -0500

    Download more fishtest games

--- scripts/download_fishtest_pgns.py
+++ scripts/download_fishtest_pgns.py
@@ -11,6 +11,6 @@ from huggingface_hub import snapshot_download
 snapshot_download(
     repo_id="official-stockfish/fishtest_pgns",
     repo_type="dataset",
-    allow_patterns="25-09-0*/*/*.pgn.gz",
+    allow_patterns="25-09-*/*/*.pgn.gz",
     local_dir=Path(__file__).resolve().parents[1] / "data" / "fishtest_pgns",
 )
